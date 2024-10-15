f3109de58e0145b58decc135864a5c3f6954a179
Srushti Redkar <srushtiredkar70@gmail.com>
2024-10-15 21:55:16 +0530

    Create get api

--- src/main/java/com/example/student/api/StudentController.java
+++ src/main/java/com/example/student/api/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
-    @GetMapping(name = "/get")
+    @GetMapping(value = "/get")
     String getDemo() {
         return "Hello friend";
     }
